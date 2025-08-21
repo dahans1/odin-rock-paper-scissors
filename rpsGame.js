db78b2e9dc439a54fbd340349f986660bf8235f0
San Dahan <sandahan007@gmail.com>
2025-08-21 12:25:51 -0400

    Write logic to play the entire game
    Move score variables and playRound function inside
    playGame function. Loop playRound for 5 rounds.

--- rpsGame.js
+++ rpsGame.js
@@ -1,6 +1,3 @@
-var humanScore = 0
-var computerScore = 0
-
 function getComputerChoice() {
     let num = Math.random()
     
@@ -18,45 +15,62 @@ function getHumanChoice() {
     return choice.toLowerCase()
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log(`Tie! Both players chose ${humanChoice}`)
-    } else {
-        switch (humanChoice) {
-            case 'rock':
-                if (computerChoice == 'paper') {
-                    console.log("You lose! Paper beats Rock")
-                    computerScore += 1
-                } else { // scissors
-                    console.log("You win! Rock beats Scissors")
-                    humanScore += 1
-                }
-                break
-            case 'paper':
-                if (computerChoice == 'rock') {
-                    console.log("You win! Paper beats Rock")
-                    humanScore += 1
-                } else { // scissors
-                    console.log("You lose! Scissors beats Paper")
-                    computerScore += 1
-                }
-                break
-            case 'scissors':
-                if (computerChoice == 'rock') {
-                    console.log("You lose! Rock beats Scissors")
-                    computerScore += 1
-                } else { // paper
-                    console.log("You win! Scissors beats Paper")
-                    humanScore += 1
-                }
-                break
-            default:
-                console.log("Invalid input from human player. Try again.")
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log(`Tie! Both players chose ${humanChoice}`)
+        } else {
+            switch (humanChoice) {
+                case 'rock':
+                    if (computerChoice == 'paper') {
+                        console.log("You lose! Paper beats Rock")
+                        computerScore += 1
+                    } else { // scissors
+                        console.log("You win! Rock beats Scissors")
+                        humanScore += 1
+                    }
+                    break
+                case 'paper':
+                    if (computerChoice == 'rock') {
+                        console.log("You win! Paper beats Rock")
+                        humanScore += 1
+                    } else { // scissors
+                        console.log("You lose! Scissors beats Paper")
+                        computerScore += 1
+                    }
+                    break
+                case 'scissors':
+                    if (computerChoice == 'rock') {
+                        console.log("You lose! Rock beats Scissors")
+                        computerScore += 1
+                    } else { // paper
+                        console.log("You win! Scissors beats Paper")
+                        humanScore += 1
+                    }
+                    break
+                default:
+                    console.log("Invalid input from human player. Try again.")
+            }
         }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+
+        playRound(humanSelection, computerSelection)
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You win! Score: ${humanScore}.`)
+    } else if (humanScore < computerScore) {
+        console.log(`You lose! Score: ${humanScore}`)
+    } else {
+        console.log(`It's a tie! Score: ${humanScore}`)
     }
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-playRound(humanSelection, computerSelection)
+playGame()
